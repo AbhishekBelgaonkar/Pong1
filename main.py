7999d6dc855f4b10a60cafe0425bcaa185243d61
AbhishekBelgaonkar <80008858+AbhishekBelgaonkar@users.noreply.github.com>
2021-03-23 10:28:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # pong game 0.3
 import pygame
 import sys
+import random
 # initialize pygame
 pygame.init()
 clock = pygame.time.Clock()
@@ -15,23 +16,48 @@ player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = (0, 0, 0)
 white = (255, 255, 255)
-   
+def ball_animation():
+    global ball_speed_x, ball_speed_y, player_score, opponent_score
+    # increments the position of the ball with +ve and -ve values
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    # -ve speed induces backwards movement
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0:
+        player_score += 1
+        winlose()
+    if ball.right >= screen_width:
+        opponent_score += 1
+        winlose()
+    if ball.colliderect(player) and ball_speed_x > 0:
+        ball_speed_x *= -1
+    if ball.colliderect(opponent) and ball_speed_x < 0:
+        ball_speed_x *= -1
 def player_animation():
     player.y += player_speed
+    opponent.y += opponent_speed
     if player.top <= 0:
         player.top = 0
     if player.bottom >= screen_height:
         player.bottom = screen_height
-    print(player.top,player.bottom)
-def opponent_animation():
-    opponent.y += opponent_speed
     if opponent.top <= 0:
         opponent.top = 0
     if opponent.bottom >= screen_height:
         opponent.bottom = screen_height
+def winlose():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2,screen_height/2)
+    ball_speed_x *= random.choice((1,-1))
+    ball_speed_y *= random.choice((1,-1))
 # the base speed of the ball
-ball_speed_x = 6
-ball_speed_y = 6
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
+#display text variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+
 # players
 player_speed = 0
 opponent_speed = 0
@@ -63,15 +89,19 @@ while True:
                 opponent_speed -= 7
             if event.key == pygame.K_w:
                 opponent_speed += 7
+    ball_animation()
     player_animation()
-    opponent_animation()
-
-
     # objects
     screen.fill(bg_color)
     pygame.draw.rect(screen, white, player)
     pygame.draw.rect(screen, white, opponent)
- 
+    pygame.draw.ellipse(screen, white, ball)
+    pygame.draw.aaline(screen, white, (screen_width/2, 0), (screen_width/2, screen_height)) # aaline stands for anti-aliasing line
+    # text
+    player_text = game_font.render(f"{player_score}", False, white) # the false is for anti-aliasing
+    opponent_text = game_font.render(f"{opponent_score}", False, white)
+    screen.blit(player_text, (1500,20)) # this function says take the background surface and draw it onto the screen and position it at (x,y)
+    screen.blit(opponent_text, (15, 20))
     # updates the game window
     pygame.display.flip()
-    clock.tick(65)
+    clock.tick(60)
